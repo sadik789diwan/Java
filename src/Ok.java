62247793e5a44abc2be7defb84ecf90a9a030c21
sdiwan <sdiwan@DEVPHP7-029.HPS.HPH.AD>
2019-08-21 18:12:30 +0530

    Pushing changes to GIT

--- src/Ok.java
+++ src/Ok.java
@@ -3,5 +3,7 @@ public class Ok {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sadik DIwan ok OKKKPK  ");
+		System.out.println("Clone repository in GT ");
+		
 	}
 }
